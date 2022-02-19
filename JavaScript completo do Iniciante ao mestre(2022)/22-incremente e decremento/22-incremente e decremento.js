5b37e9d7cd689cbe3e391025fb4b88a39a309df1
Mauri <iruam_mauri@msn.com>
2022-02-19 19:36:46 -0300

    19-02-2022 /2

---
+++ JavaScript completo do Iniciante ao mestre(2022)/22-incremente e decremento/22-incremente e decremento.js
@@ -0,0 +1,6 @@
+//- Operadores de incremento e decremento
+let i = 0;
+console.log(--i)
+console.log(i--)
+console.log(i++)
+console.log(++i)
---
+++ JavaScript completo do Iniciante ao mestre(2022)/23-Operadores de Comparação/23-Operadores de Comparação.js
@@ -0,0 +1,12 @@
+//- Operadores de Comparação
+/*
+    == igualdade de valor
+    === igualdade de valor e tipo
+    < menor que
+    > maior que
+    <= menor ou igual
+    >= maior ou igual
+    != valores diferentes
+    !== valores e tipos diferentes
+*/
+
